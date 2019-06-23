cd550fb55705fb4451f2c480d0275cac04f7603f
Ghevond <ghevond.98@gmail.com>
2019-06-23 22:43:18 +0400

    builder fix

--- MainSolution/6.Builder/HtmlElemet.h
+++ MainSolution/6.Builder/HtmlElemet.h
@@ -1,78 +1,106 @@
 #ifndef _HTML_ELEMENT_H__
 #define _HTML_ELEMENT_H__
 
+#include <iostream>
+#include <string>
 #include <vector>
 #include <sstream>
+#include <memory>
 
-class HtmlBuilder;
+struct HtmlBuilder;
 
 class HtmlElement
 {
+	friend class HtmlBuilder;
 public:
+	HtmlElement() {}
 	HtmlElement(const std::string& name, const std::string& text)
-		:_name(name),
+		: _name(name),
 		_text(text)
-	{}
-	HtmlElement(const HtmlElement& other) = default;
-	
-	~HtmlElement() {}
-
-public:
-
-	const std::string& get_tag() const {
-		return _name;
+	{
 	}
 
-	const std::string get_text()const {
-		return _text;
-	}
-
-	std::vector<HtmlElement>& get_inner_elements() {
-		return _innerElements;
-	}
-
-public:
-
-	std::string str(int indent = 0) const {
+	std::string str(int indent = 0) const
+	{
 		std::ostringstream oss;
-		std::string i(INDENT_SIZE*indent, ' ');
+		std::string i(indent_size*indent, ' ');
 		oss << i << "<" << _name << ">" << std::endl;
 		if (_text.size() > 0)
-			oss << std::string(INDENT_SIZE*(indent + 1), ' ') << _text << std::endl;
+			oss << std::string(indent_size*(indent + 1), ' ') << _text << std::endl;
 
-		for (const auto& e : _innerElements)
+		for (const auto& e : _elements)
 			oss << e.str(indent + 1);
 
 		oss << i << "</" << _name << ">" << std::endl;
 		return oss.str();
 	}
 
-	HtmlBuilder build(const std::string& root_name) {
-		return HtmlBuilder(root_name);
+public:
+
+	const std::string& get_name() const{
+		return _name;
+	}
+
+	void set_name(const std::string& name) {
+		_name = name;
+	}
+
+	const std::string& get_text() const{
+		return _text;
+	}
+
+	void set_text(const std::string& text) {
+		_text = text;
+	}
+
+private:
+
+	std::vector<HtmlElement>& get_elements(){
+		return _elements;
 	}
 
 public:
-	const int INDENT_SIZE = 2;
+
+	static HtmlBuilder* init(const std::string& root_name);
+
 private:
+
 	std::string _name;
 	std::string _text;
-	std::vector<HtmlElement> _innerElements;
+	std::vector<HtmlElement> _elements;
+	const size_t indent_size = 2;
 };
 
 class HtmlBuilder
 {
-public:
-	HtmlBuilder(const HtmlElement& root)
-		:_root(root)
-	{}
+	friend class HtmlElement;
+private:
 	HtmlBuilder(const std::string& root_name)
-		:_root(root_name,"")
-	{}
-	~HtmlBuilder(){}
+	{
+		_root.set_name(root_name);
+		_root.set_text("");
+	}
+public:
+
+	const std::string str() {
+		return _root.str();
+	}
+
+	HtmlBuilder* add_child(const std::string& child_name, const std::string& child_text) {
+		_root.get_elements().push_back({ child_name,child_text });
+		return this;
+	}
+
+	HtmlElement build() const{
+		return std::move(_root);
+	}
 
 private:
 	HtmlElement _root;
 };
 
+HtmlBuilder* HtmlElement::init(const std::string& root_name) {
+	return new HtmlBuilder(root_name);
+}
 
 #endif // !_HTML_ELEMENT_H__
--- MainSolution/6.Builder/Main.cpp
+++ MainSolution/6.Builder/Main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "HtmlElemet.h"
 
 int main() {
 
+	HtmlElement result = HtmlElement::init("html")
+		->add_child("h1", "text1")
+		->add_child("h1", "text2")
+		->add_child("h1", "text3")
+		->add_child("h1", "text4")
+		->build();
 
+	std::cout << result.str() << std::endl;
 
 	std::cin.get();
 	return 0;
